5c38c0dffb0962118677895aade4018b5dde1a76
Mahesh19 <30853110+MaheshMahi19@users.noreply.github.com>
2018-11-25 18:21:22 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commadns
+from discord.ext.commands import Bot
+import asyncio
+
+cilent = discord.Client()
+client = commands.Bot(command_prefix="*")
+
+@client.event
+async def on_ready():
+  print("I am Logged in")
+  
+@client.event
+async def on_message(message):
+  if messgae.content.startswith(*hello):
+    msg="Hello {o.author.mention}. How is ur Day".format(message)
+    await client.send_message(message.channel,msg)
+   
+  
+
+cleint.run('NTEzMzE2MTgzNTA0ODQ2ODU4.DtwsHg.AhmCqvSJqXP47x4UzL-SGXhB6rI
+')
